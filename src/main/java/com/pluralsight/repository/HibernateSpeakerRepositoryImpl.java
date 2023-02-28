ad11a26e262a2727a3385b97fea045be256b8889
Tia Theisen <Tia.Theisen@state.mn.us>
2023-02-28 15:03:24 -0600

    create seedNum variable and use @Value to manipulate speaker object

--- src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,6 +15,9 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
     @Autowired
     private Calendar cal;
 
+    @Value("#{ T(java.lang.Math).random() * 100 }") //grabs a value out of the java.lang.Math library
+    private double seedNum;
+
     public List<Speaker> findAll() {
         //declare a new array
         List<Speaker> speakers = new ArrayList<Speaker>();
@@ -21,8 +25,10 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
         Speaker speaker = new Speaker();
         speaker.setFirstName("Tia");
         speaker.setLastName("Marie");
+        speaker.setSeedNum(seedNum); //storing seedNum inside of speaker object
 
         System.out.println("cal: " + cal.getTime());
+        System.out.println("seedNum is " + seedNum);
 
         speakers.add(speaker);
 
